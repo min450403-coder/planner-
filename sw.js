72c585fe52a9ca6d8047c6509fbd170cd5c1d0ac
min450403-coder <min450403@gmail.com>
2026-09-18 13:10:27 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+javascript
+const CACHE_NAME = 'planner-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
